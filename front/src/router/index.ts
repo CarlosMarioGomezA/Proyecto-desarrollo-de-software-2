418f6d513d3cf5ca585413c898be19814215259c
Juanfran24 <xxxestebanfranco@gmail.com>
2022-04-28 23:45:16 -0500

    cambios nueva vista con ruta

--- front/src/router/index.ts
+++ front/src/router/index.ts
@@ -2,7 +2,11 @@ import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 
 
 const routes: Array<RouteRecordRaw> = [
-  
+  {
+    path: '/',
+    name: 'login',
+    component: () => import('@/views/vistaIniciarSesion.vue'),
+  }
 ]
 
 const router = createRouter({
