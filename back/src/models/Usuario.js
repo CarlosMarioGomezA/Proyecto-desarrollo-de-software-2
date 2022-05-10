14a9157671a9fa05cbb5ffd1e95fd062e63e3050
Juanfran24 <xxxestebanfranco@gmail.com>
2022-05-10 11:42:00 -0500

    pequeños ajustes para bd

--- back/src/models/Usuario.js
+++ back/src/models/Usuario.js
@@ -5,8 +5,8 @@ class Usuario {
 
     constructor(password, nombre, apellido, direccion, tipoDocumento, documento, correo, rol, telefono, estado, intentos){
         this.password_usuario = password;
-        this.nombre = nombre;
-        this.apellido = apellido;
+        this.nombres = nombre;
+        this.apellidos = apellido;
         this.direccion = direccion;
         this.tipo_documento = tipoDocumento;
         this.numero_documento = documento;
