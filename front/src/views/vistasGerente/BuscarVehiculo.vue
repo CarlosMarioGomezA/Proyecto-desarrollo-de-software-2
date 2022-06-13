88dff10c9c7ed5782dfa1ce79bfcc7e79356d475
SebasGonza <sebasgoro41@gmail.com>
2022-06-13 13:39:51 -0500

    Add vistas Vehiculo

---
+++ front/src/views/vistasGerente/BuscarVehiculo.vue
@@ -0,0 +1,24 @@
+<!--*
+ * @author SebasRome
+ *-->
+<template>
+
+<div id="BuscarVehiculo">
+    <h1>Buscar Vehiculo</h1>
+</div>
+  
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+#BuscarVehiculo{
+    margin-top: 4.5rem;
+}
+
+</style>
